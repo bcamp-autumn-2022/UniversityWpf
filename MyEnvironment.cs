0b988cfa0e545078b3d784b58e7079e43d8b5e6c
Pekka Alaluukas <Pekka.Alaluukas@oamk.fi>
2022-10-03 13:48:08 +0300

    login works

---
+++ MyEnvironment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityWpf
+{
+    internal static class MyEnvironment
+    {
+        internal static string GetBaseUrl()
+        {
+            return "https://localhost:7080/api";
+            //return "https://pekan-dotnet-university.herokuapp.com/api";
+        }
+    }
+}
